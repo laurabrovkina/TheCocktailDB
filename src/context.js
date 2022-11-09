4760b4b1b36099dc7bab3b715190b3fec6a2bcf3
laurabrovkina <laurabrovkina@gmail.com>
2022-11-09 13:00:02 +1300

    Single cocktail page

--- src/context.js
+++ src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a');
   const [cocktails, setCocktails] = useState([]);
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback(async () => {
     setLoading(true);
     try {
       const response = await fetch(`${url}${searchTerm}`);
@@ -30,11 +30,10 @@ const AppProvider = ({ children }) => {
       console.log(error);
       setLoading(false);
     }
-  }
-
+  }, [searchTerm])
   useEffect(() => {
     fetchDrinks()
-  }, [searchTerm])
+  }, [searchTerm,fetchDrinks])
 
   return (
     <AppContext.Provider
@@ -43,7 +42,7 @@ const AppProvider = ({ children }) => {
     </AppContext.Provider>
   )
 }
-// make sure use
+
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
